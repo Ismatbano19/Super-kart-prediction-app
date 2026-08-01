9edc9acb892c79207b804f0ae4c76ddf3edaf465
Ismatbano19 <ismatbano19@gmail.com>
2026-08-01 12:22:59 +0000

    Fix model filename

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import joblib
 
 # Load trained model
-model = joblib.load("best_model.pkl")   # Replace with your saved model name
+model = joblib.load("prediction_model_v1_0.joblib")   # Replace with your saved model name
 
 st.title("SuperKart Sales Prediction")
 st.write("Enter the product and store details to predict product sales.")
